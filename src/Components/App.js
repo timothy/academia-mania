f94d139b19603db786326fbf1ec1457d86536384
timothy <t192.168.1.0@gmail.com>
2019-10-26 20:30:05 -0700

    simplified App.js and made it conform to the pattern of other components

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,24 +1,19 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import {Header, Footer, Body, Posts} from "./Layouts"
 
-export default class extends Component {
-    render() {
-        return (
-            <BrowserRouter>
-                <Header>
-                    <Switch>
-                        <Route path="/" exact component={Body} />
-                        <Route path="/posts/:id" exact component={Posts} />
-                        <Route path="/posts" exact component={Posts} />
-                        <Route path="/1" exact component={Footer} />
-                        <Route path="/" render={(props) => {
-                            console.log(props.location);
-                            return (<div>404 Path "{props.location.pathname}" not found</div>)
-                        }} />
-                    </Switch>
-                </Header>
-            </BrowserRouter>
-        )
-    }
-}
+export default props =>
+    <BrowserRouter>
+        <Header>
+            <Switch>
+                <Route path="/" exact component={Body}/>
+                <Route path="/posts/:id" exact component={Posts}/>
+                <Route path="/posts" exact component={Posts}/>
+                <Route path="/1" exact component={Footer}/>
+                <Route path="/" render={(props) => {
+                    console.log(props.location);
+                    return (<div>404 Path "{props.location.pathname}" not found</div>)
+                }}/>
+            </Switch>
+        </Header>
+    </BrowserRouter>
