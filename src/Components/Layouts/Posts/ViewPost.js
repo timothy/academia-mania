fa24a283bf73bf488a07012f76bc4117af3cbaca
timothy <t192.168.1.0@gmail.com>
2019-11-16 20:48:10 -0800

    view post will now display actual posts

--- src/Components/Layouts/Posts/ViewPost.js
+++ src/Components/Layouts/Posts/ViewPost.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {Link, useParams} from "react-router-dom";
-
+import {myState} from '../../../PubSub/pub-sub'
 
 export default props => {
     let {id} = useParams();
 
+    console.log(myState);
+
     return <div>
         <h1>View Posts Page ID:{id}</h1>
+        <div dangerouslySetInnerHTML={{ __html: myState.posts[id].renderedHTML }}></div>
         <Link to="/">Go to home</Link>
         {console.log(props.match)}
     </div>
