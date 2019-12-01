919b551bc7dd408abb760678b2850cef36c60c65
timothy <t192.168.1.0@gmail.com>
2019-11-30 21:57:24 -0800

    ViewPosts.js: added PostReply component.

--- src/Components/Layouts/Posts/ViewPost.js
+++ src/Components/Layouts/Posts/ViewPost.js
@@ -5,6 +5,7 @@ import {myState} from '../../../PubSub/pub-sub';
 import 'medium-draft/lib/index.css';
 import 'medium-draft/lib/basic.css';
 import 'isomorphic-fetch';
+import PostReply from './PostReply'
 
 export default props => {
     let id = (useParams().id && myState.posts[useParams().id]) ? useParams().id : 0;
@@ -17,6 +18,7 @@ export default props => {
         <Box p={2} bgcolor="background.paper">
             <div dangerouslySetInnerHTML={{__html: myState.posts[id].renderedHTML}}></div>
         </Box>
+        {PostReply()}
         <Link to="/">Go to home</Link>
         {console.log(props.match)}
     </div>
